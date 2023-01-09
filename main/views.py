0a110d337b2ddf6ccd25c5fe5064a3fbcc1f1397
niluzunoglu <47059818+niluzunoglu@users.noreply.github.com>
2023-01-09 23:15:02 +0300

    static files added

--- main/views.py
+++ main/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    
-    return render(request,"main/home.html",{"data":"okay"})
+    return render(request,"main/home.html")
