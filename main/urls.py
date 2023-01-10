794452e6a028af8a1bee647438ed562ed9638e8b
niluzunoglu <47059818+niluzunoglu@users.noreply.github.com>
2023-01-10 23:59:05 +0300

    get_text func added

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('text',views.get_text,name="get_text")
 ]
--- main/views.py
+++ main/views.py
@@ -3,5 +3,8 @@ import requests
 
 def home(request):
 
-    
+
     return render(request,"main/home.html",{"data":"a"})
+
+def get_text(request):
+    return render(request,"main/text_input.html")
