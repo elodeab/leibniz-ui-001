e076bdfd3e1e2fdddc8a4cd7dcc12c7327f4d05c
niluzunoglu <47059818+niluzunoglu@users.noreply.github.com>
2023-01-10 23:41:47 +0300

    html files edited

--- main/views.py
+++ main/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    return render(request,"main/home.html")
+    
+    
+    return render(request,"main/home.html",{"data":"a"})
